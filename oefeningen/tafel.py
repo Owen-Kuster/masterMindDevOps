84fd2420f531ad7b6ab1fa1bd6dea7b495d1e783
Owen Kuster <owenkuster08@gmail.com>
2026-06-09 14:26:46 +0200

    Add flake8 lint workflow

--- oefeningen/tafel.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python3
-# tafel.py
-
-keuze = int(input("Welke tafel? (1-10): "))
-
-for i in range(1, 11):  # 1 t/m 10
-    print(keuze, "x", i, "=", keuze * i)
--- oefeningen/weektabel.py
+++
@@ -1,5 +0,0 @@
-#!/bin/python3
-# weektabel.py
-
-for getal in range(1, 8):  # 1 t/m 7
-    print(getal, "x 7 =", getal * 7)
